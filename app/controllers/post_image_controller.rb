f866b33c8d4f00c222cf5d4a81c25346d06e62ab
umiiii419 <umiiii419@gmail.com>
2021-07-21 06:52:01 +0000

    [Add] createメソッド作成

--- app/controllers/post_image_controller.rb
+++ app/controllers/post_image_controller.rb
@@ -5,6 +5,10 @@ class PostImageController < ApplicationController
   end
 
   def create
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id
+    @post_image.save
+    redirect_to post_images_path
   end
 
   def index
