f0849b312f91021270b291d7168ced583dd20bf6
umiiii419 <umiiii419@gmail.com>
2021-07-26 07:53:32 +0000

    [Add] Refile.secret_keyのエラーが表示されたため、キー情報を設定

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '91c13ad4dcb6fe44c908eb711f79d2eb496afb18c94b226ef08db401d3f05ce8143bf361168a078420adb84cfbcd01158721a8ef7a908f502fe8cb5310c0c1c7'
