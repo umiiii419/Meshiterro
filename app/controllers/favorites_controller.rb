a8a96986d4958cc6e43f5577006c6b7887dc6c4b
umiiii419 <umiiii419@gmail.com>
2021-07-28 23:07:59 +0000

    [Add]4-4 favorites_controller.rbに、createアクションとdestroyアクションの中身を実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -10,6 +10,9 @@ class FavoritesController < ApplicationController
 
 
   def destroy
+    post_image = PostImage.find(params[:post_image_id])
+    favorite = current_user.favorites.find_by(post_image_id: post_image.id)
+    favorite.destroy
+    redirect_to post_image_path(post_image)
   end
-
 end
