530433597cfd28dcb8b97a565f9919ab78e7a366
umiiii419 <umiiii419@gmail.com>
2021-07-29 01:09:17 +0000

    [Add]4-5 updateアクションと中身を記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,14 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
 
+
+
+  
 end
