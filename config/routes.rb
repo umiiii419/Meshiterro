91654555a61e924eb76f55020f3223f3bf4e1333
umiiii419 <umiiii419@gmail.com>
2021-07-29 06:03:11 +0000

    [Fix]4-6 usersを修正

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
     resources :post_comments, only: [:create, :destroy]
   end
 
-  resources :users, only: [:show, :edit :update]
+  resources :users, only: [:show, :edit, :update]
 end
