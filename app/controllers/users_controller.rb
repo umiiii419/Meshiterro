a53832d98a915b49048985affa7f4e9b4f0284f1
umiiii419 <umiiii419@gmail.com>
2021-07-29 01:11:10 +0000

    [Add]4-5 Strong Parametersを記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
@@ -15,6 +15,9 @@ class UsersController < ApplicationController
   end
 
 
+  private
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
 
-  
 end
